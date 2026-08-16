210cd31110f787cc770d577ec7f72923a98637ee
lahirutheekshana <lahirutheekshanabandara2002@gmail.com>
2026-08-16 16:12:06 +0530

    test 1

---
+++ ISO-Core-App/ISO-Core-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ISO-Core-App
+//
+//  Created by student1 on 2026-08-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ISO-Core-App/ISO-Core-App/ISO_Core_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISO_Core_AppApp.swift
+//  ISO-Core-App
+//
+//  Created by student1 on 2026-08-16.
+//
+
+import SwiftUI
+
+@main
+struct ISO_Core_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
